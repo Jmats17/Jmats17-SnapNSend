b0cea4e2bb401fc1c2d3c51691597999810cd0a7
justinmatsnev <jmatsnev@gmail.com>
2020-04-04 13:41:58 -0400

    first commit

---
+++ SnapNSend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapNSend
+//
+//  Created by Justin Matsnev on 4/4/20.
+//  Copyright © 2020 Justin Matsnev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
